bdd52e73390ae21e33854fcc660214cdba178029
Scott Leberknight <scottleber@gmail.com>
2015-07-30 18:37:13 -0400

    Add Javadoc to SpringContextBuilder

--- src/main/java/com/fortitudetec/spring/context/SpringContextBuilder.java
+++ src/main/java/com/fortitudetec/spring/context/SpringContextBuilder.java
@@ -15,29 +15,72 @@ import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkState;
 
+/**
+ * Builder class for easily constructing Spring {@link ApplicationContext} instances programmatically using either
+ * XML or annotation-based configuration. The generated {@code ApplicationContext} instance is built by creating a
+ * parent context, which contains registered singleton beans, and then a child context. This allows specific
+ * singleton beans to be accessible from the child context, e.g. in Dropwizard applications a ManagedDataSource or
+ * a Configuration object. The parent context beans can be referenced in either XML or annotation configurations.
+ * <p>
+ * The methods in this class return an instance of this class, so they can be chained together in a builder-style.
+ * Once the configuration is complete, call {@link #build()} to create the {@code ApplicationContext}.
+ * <p>
+ * If you need to combine XML and annotation configurations, you can create a shell configuration class which uses
+ * the {@code @ImportResource} annotation to specify the XML configuration file to import, and then add the
+ * shell class as an annotated configuration class. For example:
+ * <pre>
+ * {@literal @}Configuration
+ * {@literal @}ImportResource("applicationContext.xml")
+ *  public class XmlImportingConfiguration {
+ *  }
+ * </pre>
+ */
 public class SpringContextBuilder {
 
     private final Map<String, Object> _parentContextBeans;
     private final List<Class> _annotatedClasses;
     private final List<String> _configLocations;
 
+    /**
+     * Create a context builder.
+     */
     public SpringContextBuilder() {
         _parentContextBeans = Maps.newHashMap();
         _annotatedClasses = Lists.newArrayList();
         _configLocations = Lists.newArrayList();
     }
 
+    /**
+     * Adds the specified bean to the parent context.
+     *
+     * @param name the bean name
+     * @param bean the bean instance
+     * @return the builder instance
+     */
     public SpringContextBuilder addParentContextBean(String name, Object bean) {
         _parentContextBeans.put(name, bean);
         return this;
     }
 
+    /**
+     * Adds an annotation-based Spring {@literal @}Configuration class.
+     *
+     * @param aClass class containing Spring configuration. Should be a class annotated with {@literal @}Configuration.
+     * @return the builder instance
+     */
     public SpringContextBuilder addAnnotationConfiguration(Class aClass) {
         checkConfigLocationsIsEmpty();
         _annotatedClasses.add(aClass);
         return this;
     }
 
+    /**
+     * Adds multiple annotation-based Spring {@literal @}Configuration classes.
+     *
+     * @param classes the classes containing Spring configuration. Should be classes annotated with
+     *                {@literal @}Configuration.
+     * @return the builder instance
+     */
     public SpringContextBuilder withAnnotationConfigurations(Class... classes) {
         checkConfigLocationsIsEmpty();
         Collections.addAll(_annotatedClasses, classes);
@@ -49,12 +92,24 @@ public class SpringContextBuilder {
                 "XML config locations have already been specified - annotated classes cannot be added!");
     }
 
+    /**
+     * Adds a single Spring XML configuration location.
+     *
+     * @param location the XML config location, e.g. {@code applicationContext.xml}
+     * @return the builder instance
+     */
     public SpringContextBuilder addXmlConfigLocation(String location) {
         checkAnnotatedClassesIsEmpty();
         _configLocations.add(location);
         return this;
     }
 
+    /**
+     * Adds multiple Spring XML configuration locations.
+     *
+     * @param locations the XML config locations, e.g. {@code applicationContext-core.xml, applicationContext-dao.xml}
+     * @return the builder instance
+     */
     public SpringContextBuilder withXmlConfigLocations(String... locations) {
         checkAnnotatedClassesIsEmpty();
         Collections.addAll(_configLocations, locations);
@@ -66,6 +121,11 @@ public class SpringContextBuilder {
                 "Annotated classes have already been specified - XML config locations cannot be added!");
     }
 
+    /**
+     * Generate the {@code ApplicationContext}.
+     *
+     * @return the {@code ApplicationContext} defined by this builder
+     */
     public ApplicationContext build() {
         ApplicationContext parent = buildParentContext();
         return buildContext(parent);
